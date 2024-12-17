03eeef4ed94e7a800a85bb8e4df73bd7fc9d7a66
Monika Mak <monika1986mak@gmail.com>
2024-12-17 22:19:42 +0000

    fix naming issue, add path

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_arth_beneath.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_art_beneath.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
