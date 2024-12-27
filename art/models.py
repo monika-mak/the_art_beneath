a41ca3ef1cefbb09928a715ece186730b0097920
Monika Mak <monika1986mak@gmail.com>
2024-12-27 03:14:31 +0000

    create an art app and categories

--- art/models.py
+++ art/models.py
@@ -2,27 +2,22 @@ from django.db import models
 
 
 class Category(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-        
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    def get_friendly_name(self):
+    
+    def get_display_name(self):
         return self.display_name
 
-
-class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+class Art(models.Model):
+    Category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    meaning = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
