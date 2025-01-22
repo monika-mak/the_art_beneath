b743578e5fada8ebbeaa276ef5fe0196038020cd
Monika Mak <monika1986mak@gmail.com>
2025-01-22 17:36:45 +0000

    add images to models

--- art/models.py
+++ art/models.py
@@ -29,6 +29,11 @@ class Art(models.Model):
     price = models.IntegerField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    image_2_url= models.URLField(max_length=1024, null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
+    
+
