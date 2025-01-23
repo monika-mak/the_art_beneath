f2ccac3302915c2ed7794d1860cd73c68240fa81
Monika Mak <monika1986mak@gmail.com>
2025-01-23 14:19:17 +0000

    add product detial template

--- art/urls.py
+++ art/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_art, name='art')
+    path('', views.all_art, name='art'),
+    path('<art_id>', views.art_detail, name='art_detail'),
 ]
