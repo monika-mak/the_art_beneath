a85b5f3bfe7a07f45519760acda832b11e941778
Monika Mak <monika1986mak@gmail.com>
2025-01-25 23:42:24 +0000

    fix issue, repalce images with the right format

--- the_art_beneath/urls.py
+++ the_art_beneath/urls.py
@@ -26,3 +26,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('art/', include('art.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
