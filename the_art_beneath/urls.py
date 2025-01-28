eebe5bc570986f8563fc01384983cbc8f6a9e428
Monika Mak <monika1986mak@gmail.com>
2025-01-28 15:22:08 +0000

    add bag template

--- the_art_beneath/urls.py
+++ the_art_beneath/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('art/', include('art.urls')),
+    path('bag  /', include('bag.urls')),
+ 
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # code to use static files when in development mode
