d4d3e84a9254c528ca088c2a4d4be2953cfca2fa
Monika Mak <monika1986mak@gmail.com>
2025-01-29 06:04:02 +0000

    display shopping bag, set delivery logic

--- the_art_beneath/settings.py
+++ the_art_beneath/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # `allauth` needs this from django
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents' # make bag available across all apps
             ],
         },
     },
@@ -174,6 +175,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+FREE_DELIVERY_THRESHOLD = 0
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 
 # Default primary key field type
