cccb3a559f138c64d626d5f8cbecdd5c4e7f834f
Monika <monika1986mak@gmail.com>
2025-01-31 15:01:13 +0000

    display messages to user when updating and deleting bag

--- bag/views.py
+++ bag/views.py
@@ -14,17 +14,17 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified art to the shopping bag """
-    art = Art.objects.get(pk=item_id)
+    art = get_object_or_404(Art, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated "{art.name}" quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
-
-        messages.success(request, f'Added {art.name} to your bag')
+        messages.success(request, f'Added "{art.name}" to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -32,14 +32,16 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified art to the specified amount"""
-
+    art = get_object_or_404(Art, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated "{art.name}" quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed "{art.name}" from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -53,7 +55,7 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-        messages.success(request, f'Removed {art.name} from your bag')
+        messages.success(request, f'Removed "{art.name}" from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
