30a1ee01387fe0d2985de695128fb707ccfb72a7
Monika <monika1986mak@gmail.com>
2025-02-05 00:53:41 +0000

    add checkout sucess, made payment creation done while in the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,63 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from art.models import Art
 from bag.contexts import bag_contents
 
 import stripe
 
 
 def checkout(request):
-
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-
     bag = request.session.get('bag', {})
+
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('art'))
+
+    if request.method == 'POST':
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    art = Art.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        art=art,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+                except Art.DoesNotExist:
+                    messages.error(request, (
+                        "One of the art in your bag wasn't found in our database. "
+                        "Please call us for assistance!"
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. Please double check your information.')
+    else:
+        order_form = OrderForm()
+
+    # PaymentIntent creation is always done in the checkout page.
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
@@ -26,12 +67,8 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-    order_form = OrderForm()
-
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
@@ -39,12 +76,26 @@ def checkout(request):
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
+    return render(request, template, context)
 
-    # template = 'checkout/checkout.html'
-    # context = {
-    #     'order_form': order_form,
-    #     'stripe_public_key': 'pk_test_51QnTQ5LDUEz8eXIQZKuBkJmEU35DeUan4md0Ltt9sC1RcPDjjHXSjFOx1Qzh01ckAUQ03vdvLt4rKJSeJBdm1bBS00AevoNSXI',
-    #     'client_secret': 'test client secret',
-    # }
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
 
     return render(request, template, context)
