d23edc42b450cecb0c59cd64761a4fa228e16e72
Monika <monika1986mak@gmail.com>
2025-02-14 21:22:10 +0000

    Revert "add slug to models"
    This reverts commit 9911f31d20589ea33928d1267cca7fd85c01768c.

--- art/models.py
+++ art/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -21,6 +20,7 @@ class Art(models.Model):
     class Meta:
         verbose_name_plural = 'Art'
 
+    # code used from https://docs.djangoproject.com/en/5.1/ref/models/fields/#choices
     ORIENTATION_CHOICES = [
         ('horizontal', 'Horizontal'),
         ('vertical', 'Vertical'),
@@ -34,7 +34,6 @@ class Art(models.Model):
     ]
 
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    slug = models.SlugField(unique=True, blank=True, null=True)  # Ensuring unique, but allowing blank during migration
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -44,28 +43,20 @@ class Art(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    image_2_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_2_url= models.URLField(max_length=1024, null=True, blank=True)
     image_2 = models.ImageField(null=True, blank=True)
-    orientation = models.CharField(max_length=25, choices=ORIENTATION_CHOICES, default='square')
-    size = models.CharField(max_length=25, choices=SIZE_CHOICES, default='140x140cm')
-
-    def save(self, *args, **kwargs):
-        """ Automatically generate slug from name, ensuring uniqueness. """
-        if not self.slug:
-            base_slug = slugify(self.name)
-            slug = base_slug
-            counter = 1
-            while Art.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
+    orientation = models.CharField(
+        max_length=25,
+        choices=ORIENTATION_CHOICES, default='square')
+    size = models.CharField(
+        max_length=25,
+        choices=SIZE_CHOICES, default='140x140cm'
+        )
 
     def __str__(self):
         return self.name
 
 
-    def __str__(self):
-        return self.name
+
 
 
--- art/urls.py
+++ art/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('add/', views.add_art, name='add_art'),
     path('edit/<int:art_id>/', views.edit_art, name='edit_art'),
     path('delete/<int:art_id>/', views.delete_art, name='delete_art'),
-    # path('art/<slug:slug>/', views.art_detail, name='art_detail'),
 ]
