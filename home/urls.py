6a6e16372e0ecd8985489c138817ba9d6ee474bc
Monika <monika1986mak@gmail.com>
2025-02-20 03:43:39 +0000

    display about page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import artist_detail
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
+    path('artist/', artist_detail, name='artist_detail'),
 ]
