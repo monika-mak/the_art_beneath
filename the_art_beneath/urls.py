d3610c1d9361eb0c31e89b56cbfac3bdee8f6bfd
Monika <monika1986mak@gmail.com>
2025-02-24 00:43:13 +0000

    add sitemap and robot.txt files

--- the_art_beneath/urls.py
+++ the_art_beneath/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -41,3 +42,5 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
+handler404 = 'the_art_beneath.views.handler404'
