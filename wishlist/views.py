a123df15cb1246da434909b1201fe4fa7e91d2d4
Monika <monika1986mak@gmail.com>
2025-02-25 19:55:41 +0000

    remove item from wishlist instead of deleting item fully, display message during wishlist actions

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Wishlist
 from art.models import Art
 
@@ -13,13 +14,16 @@ def wishlist_view(request):
 def add_to_wishlist(request, art_id):
     art = get_object_or_404(Art, id=art_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+    messages.info(request, f'Added {art.name} to your wishlist')
     wishlist.items.add(art)
     return redirect('wishlist')
+
 
 
 @login_required
 def remove_from_wishlist(request, art_id):
     art = get_object_or_404(Art, id=art_id)
     wishlist = get_object_or_404(Wishlist, user=request.user)
+    messages.info(request, f'Removed {art.name} from your wishlist')
     wishlist.items.remove(art)
     return redirect('wishlist')
